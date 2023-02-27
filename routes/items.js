af1e0bb299d18ee72f65268712f41107f07907b1
Harsh Rudani🚀 <harsh.rudani@itinformatix.com>
2023-02-27 12:00:16 +0530

    aa

--- routes/items.js
+++ routes/items.js
@@ -9,6 +9,8 @@ let data = [
     { id: 3, title: 'Write new article', order: 3, completed: true, createdOn: new Date() },
     { id: 4, title: 'Walk toward home', order: 4, completed: false, createdOn: new Date() },
     { id: 5, title: 'Have some dinner', order: 5, completed: false, createdOn: new Date() },
+    { id: 6, title: 'Have some dinner', order: 5, completed: false, createdOn: new Date() },
+    { id: 7, title: 'Have some dinner', order: 5, completed: false, createdOn: new Date() },
 ];
 
 // HTTP methods ↓↓ starts here.
